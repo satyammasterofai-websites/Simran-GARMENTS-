1d0493f03ade52a32e40f3c837dc80bc8b22eb63
satyammasterofai-websites <satyammasterofai@gmail.com>
2026-05-31 16:59:15 +0530

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -15,7 +15,10 @@ export function About() {
             className="relative"
           >
             <div className="aspect-[4/5] relative rounded-2xl overflow-hidden shadow-2xl">
-              <Image           'https://images.unsplash.com/photo-1778754607413-168eff59b5dd?q=80&w=327&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              <Image                                  
+
+'https://images.unsplash.com/photo-1778754607413-168eff59b5dd?q=80&w=327&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+
                 alt="Simran Garments Store Overview"
                 fill
                 className="object-cover"
